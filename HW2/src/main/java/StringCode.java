b6b9ff7172425142b7a29b8d70225d86989e5cc3
NDH2611 <hoadztv11@gmail.com>
2025-09-28 21:23:13 +0700

    StringCode

--- HW2/src/main/java/StringCode.java
+++ HW2/src/main/java/StringCode.java
@@ -1,39 +1,65 @@
 import java.util.HashSet;
 import java.util.Set;
 
-// CS108 HW1 -- String static methods
-
 public class StringCode {
-
-	/**
-	 * Given a string, returns the length of the largest run.
-	 * A a run is a series of adajcent chars that are the same.
-	 * @param str
-	 * @return max run length
-	 */
-	public static int maxRun(String str) {
-		return 0; // YOUR CODE HERE
-	}
-
-	
-	/**
-	 * Given a string, for each digit in the original string,
-	 * replaces the digit with that many occurrences of the character
-	 * following. So the string "a3tx2z" yields "attttxzzz".
-	 * @param str
-	 * @return blown up string
-	 */
 	public static String blowup(String str) {
-		return null; // YOUR CODE HERE
+		StringBuilder s = new StringBuilder();
+		int n = str.length();
+		for (int i = 0; i < n; i++) {
+			char c = str.charAt(i);
+			if (Character.isDigit(c) && i + 1 < n) {
+				char ch = str.charAt(i + 1);
+				if (Character.isLetter(ch)) {
+					int count = c - '0';
+					for (int j = 0; j < count; j++) {
+						s.append(ch);
+					}
+				}
+			}
+			if (Character.isLetter(c)) {
+				s.append(c);
+			}
+		}
+
+		return s.toString();
 	}
-	
-	/**
-	 * Given 2 strings, consider all the substrings within them
-	 * of length len. Returns true if there are any such substrings
-	 * which appear in both strings.
-	 * Compute this in linear time using a HashSet. Len will be 1 or more.
-	 */
+
+	public static int maxRun(String str) {
+		if (str == null || str.isEmpty()) return 0;
+		int max = 1;
+		int count = 1;
+		int n = str.length();
+		for (int i = 0; i < n; i++)
+		{
+			if (i + 1 < n && str.charAt(i) == str.charAt(i+1)) {
+				count++;
+			}
+			else {
+				count = 1;
+			}
+			if (max < count) {
+				max = count;
+			}
+		}
+		return max;
+	}
+
 	public static boolean stringIntersect(String a, String b, int len) {
-		return false; // YOUR CODE HERE
+		if (len > a.length() || len > b.length()) {
+			return false;
+		}
+		Set<String> seen = new HashSet<>();
+		for (int i = 0; i < a.length() - len; i++)
+		{
+			seen.add(a.substring(i, i + len));
+		}
+		for (int i = 0; i < b.length() - len; i++)
+		{
+			String s = b.substring(i, i + len);
+			if (seen.contains(s)) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
